cbc63ff71517cb41327009c9f91b2acdf449492a
Avinash Kumar <itsmeatg@outlook.com>
2021-08-26 17:53:14 +0530

    Implementing Async and await

--- server/router/auth.js
+++ server/router/auth.js
@@ -10,6 +10,32 @@ router.get('/',(req,res)=>{
 });
 
 
+// router.post('/signup',(req,res)=>{
+//     const {name,email,phone,work,password,cpassword}=req.body;
+//     if(!name|| !email || !phone || !work|| !password || !cpassword){
+//         return res.status(422).json({error:" Please Fill requierd field"});
+        
+//     }
+
+//     User.findOne({email:email})
+//     .then((userExist)=>{
+//         if(userExist) {
+//             return res.status(422).json({error: "Email already Exist"});
+//         }
+//         // When key and value are same then no need to write name: name , instead write name
+
+//         const user = new User({name,email,phone,work,password,cpassword});
+        
+//         user.save().then(()=>{
+//             res.status(201).json({message: "Registration Succcessful"});
+
+//         }).catch((err) => res.status(500).json({error: "Failed to register"}));
+
+//     }).catch(err=>{console.log("Error in database")});
+
+
+// });
+
 router.post('/signup',(req,res)=>{
     const {name,email,phone,work,password,cpassword}=req.body;
     if(!name|| !email || !phone || !work|| !password || !cpassword){
@@ -17,22 +43,25 @@ router.post('/signup',(req,res)=>{
         
     }
 
-    User.findOne({email:email})
-    .then((userExist)=>{
-        if(userExist) {
-            return res.status(422).json({error: "Email already Exist"});
-        }
-        // When key and value are same then no need to write name: name , instead write name
+    try {
+        const userExist = await User.findOne({email:email});
+        if(userExist){
+            return res.status(422).json({error: "Email Already Exist"});
+       }
 
-        const user = new User({name,email,phone,work,password,cpassword});
+       const user = new User({name,email,phone,work,password,cpassword});
+       
+       await user.save();
+       res.status(201).json({message: "Sucessfully Registered"});
+
         
-        user.save().then(()=>{
-            res.status(201).json({message: "Registration Succcessful"});
-
-        }).catch((err) => res.status(500).json({error: "Failed to register"}));
-
-    }).catch(err=>{console.log("Error in database")});
-
+    } catch (error) {
+        console.log(error);
+        
+    }
+  
+      
+    
 
 });
 
